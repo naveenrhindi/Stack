a33c2b342f56d00e0ec211ecd9b455910600e8d4
naveenrhindi <naveenhindi4@gmail.com>
2024-09-17 13:22:49 +0530

    Create ValidP.java

---
+++ ValidP.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+class ValidP {
+    
+    public boolean isValid(String s) {
+        int n = s.length();
+        if(n%2 != 0) return false;
+        
+        Stack<Character> st = new Stack<>();
+        
+        for(int i=0;i<n;i++) {
+            char ch = s.charAt(i);
+            if(ch=='(' || ch=='[' || ch=='{') {
+                st.push(ch);
+            }else {
+                if(st.isEmpty()){
+                    return false;
+                }
+                char top = st.peek();
+                if(ch==')' && top!='(') return false;
+                else if(ch==']' && top!='[') return false;
+                else if(ch=='}' && top!='{') return false;
+                else {
+                    st.pop();
+                }
+            }
+        }
+        return (st.size()==0);
+    }
+    
+    
+    public static void main(String[] args) {
+       
+       ValidP obj = new ValidP();
+       System.out.println(obj.isValid("(){[]}"));
+       
+    }
+}
